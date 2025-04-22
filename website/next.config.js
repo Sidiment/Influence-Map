ce4210a0cbeb502e1ac026147fef5d6f894a0e67
Sidiment <yuhengz@usc.edu>
2025-04-22 13:18:21 -0700

    Move website code to dedicated folder

---
+++ website/next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'i0.hdslb.com', 'i1.hdslb.com', 'i2.hdslb.com'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig; 
