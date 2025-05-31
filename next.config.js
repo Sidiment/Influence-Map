24be497c3238b2d6414c94e3003c7f0536d1d528
Sidiment <yuhengz@usc.edu>
2025-05-30 23:38:50 -0700

    chore: Add GitHub Pages deployment configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Influence-Map',
+}
+
+module.exports = nextConfig 
